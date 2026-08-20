a322e4e3613e1a23bb4910f768b9db94c97a00a2
unknown <xiaohuaaa06@gmail.com>
2026-08-20 10:00:20 +0800

    Update database configuration

--- db.php
+++ db.php
@@ -1,12 +1,11 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "Whatpass@1113";
-$database = "toilet_monitoring";
-$port = 3307;
+define('DB_HOST', 'localhost');
+define('DB_USER', 'synergy1_yenping');
+define('DB_PASSWORD', 'R.zb0ZwEuGZ}*fW2');
+define('DB_NAME', 'synergy1_lawlifang_toilet');
 
-$conn = new mysqli($host, $user, $password, $database, $port);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
